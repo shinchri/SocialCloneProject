32dfdbd6a8af5431e04384d30eaa3dbb63007b84
Unknown <shinchri7@gmail.com>
2018-03-24 17:30:43 -0500

    created SignUp class inside views
    accounts/views.py

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -5,5 +5,9 @@ from django.views.generic import CreateView
 from . import forms
 # Create your views here.
 
-class SignUP(CreateView):
-    pass
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    # we use reverse_lazy instead of reverse because we need to wait
+    #     until the forms are set up properly
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
