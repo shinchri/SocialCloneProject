b4e5c19b78118b6c436bf2c284a9ccd57696bc91
Unknown <shinchri7@gmail.com>
2018-03-27 12:10:20 -0500

    updated admin.py for Group App to include Group and GroupMember

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+# TabularInline class - The admin interface has the ability to edit models on the same page as a parent model.
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
