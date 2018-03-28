b4ae3a7ba0f37b3c96d3c16dd7d2152788d4215f
Unknown <shinchri7@gmail.com>
2018-03-28 08:44:29 -0500

    created url routings for posts app
    posts/urls.py

---
+++ simplesocial/posts/urls.py
@@ -0,0 +1,16 @@
+# POSTS URL.py
+
+from django.urls import path
+
+from posts import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='all'),
+    path('news/', views.CreatePost.as_view(), name='create'),
+    re_path(r'^by/(?P<username>[-\w]+)/$', views.UserPosts.as_view(), name='for_user'),
+    re_path(r'^by/(?P<username>[-\w]+)/(?P<pk>\d)/$', views.PostDetail.as_view(), name='single'),
+    re_path(r'^delete/(?P<pk>\d)/$', views.DeletePost.as_view(), name='delete'),
+
+]
